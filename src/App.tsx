e1e318dc58ecf12a54765ae14a5258a3cb1fe6dc
Jeferson Mesquita <je_2742@hotmail.com>
2022-11-15 19:55:36 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header"
 import { GlobalStyles } from "./styles/GlobalStyles"
 
 export function App() {
   return (
     <div>
       <GlobalStyles />
-      <h1>Hello World</h1>
+      <Header />
     </div>
   )
 }
